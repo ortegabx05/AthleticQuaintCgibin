51f0e68eee9a01b3e6a4ecaa9e4b04987fe05978
ortegabx05 <30540228+ortegabx05@users.noreply.github.com>
2023-09-07 14:49:42 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Lyrics {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Now, this is a story all about how");
+    Console.WriteLine ("My life got flipped-turned upside down");
+    Console.WriteLine ("And I'd like to take a minute");
+    Console.WriteLine ("Just sit right there");
+    Console.WriteLine ("I'll tell you how I became the prince of a town called Bel-Air");
+  }
+}
